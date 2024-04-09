4fb61cdf6cebdb7b16e33445a008df58763d23c6
Sebastian Maniak <sebastian@maniak.io>
2024-04-09 00:55:12 -0400

    add ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "my_repository" {
-  name                 = "simple-task-app" 
+  name                 = "sebby-simple-task-app" 
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
