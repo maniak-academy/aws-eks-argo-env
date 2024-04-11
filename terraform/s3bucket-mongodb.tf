8e06e4273d79e02ef2ce63d8c1cdbf1120f9793e
Sebastian Maniak <sebastian@maniak.io>
2024-04-11 18:36:49 -0400

    Update s3bucket-mongodb.tf

--- terraform/s3bucket-mongodb.tf
+++ terraform/s3bucket-mongodb.tf
@@ -1,1 +1,14 @@
+resource "aws_s3_bucket" "backup_bucket" {
+  bucket = "sebbycorp-mongodb-backups3" # Ensure this is globally unique
+
+  # Prevent accidental bucket destruction
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  # Optionally, include tags directly here if not using a variable
+  tags = merge(
+    var.tags
+  )
+}
 
