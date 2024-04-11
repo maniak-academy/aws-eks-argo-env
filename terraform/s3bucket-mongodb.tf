945e6639a182a3121c90a6922b89645f007c907d
Sebastian Maniak <sebastian@maniak.io>
2024-04-11 16:22:11 -0400

    try again

--- terraform/s3bucket-mongodb.tf
+++ terraform/s3bucket-mongodb.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "backup_bucket" {
   bucket = "sebbycorp-mongodb-backups3" # Ensure this is globally unique
-  acl    = "public-read"
 
   # Prevent accidental bucket destruction
   lifecycle {
@@ -8,10 +7,9 @@ resource "aws_s3_bucket" "backup_bucket" {
   }
 
   # Optionally, include tags directly here if not using a variable
-  tags = {
-    "Name" = "MongoDB Backup Bucket"
-    "Environment" = "Production"
-  }
+  tags = merge(
+    var.tags
+  )
 }
 
 resource "aws_s3_bucket_policy" "backup_bucket_policy" {
