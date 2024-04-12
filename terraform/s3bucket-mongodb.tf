5a2e217142ff385f108cef9ff5dd664dc73b7eea
Sebastian Maniak <sebastian@maniak.io>
2024-04-12 14:39:57 -0400

    cleaning up s3 bucket

--- terraform/s3bucket-mongodb.tf
+++ terraform/s3bucket-mongodb.tf
@@ -1,20 +1,20 @@
-resource "aws_s3_bucket" "backup_bucket" {
-  bucket = "sebbycorp-mongodb-backups3" # Ensure this is globally unique
+# resource "aws_s3_bucket" "backup_bucket" {
+#   bucket = "sebbycorp-mongodb-backups3" # Ensure this is globally unique
 
-  # Prevent accidental bucket destruction
-  lifecycle {
-    prevent_destroy = true
-  }
+#   # Prevent accidental bucket destruction
+#   lifecycle {
+#     prevent_destroy = true
+#   }
 
-  # Optionally, include tags directly here if not using a variable
-  tags = merge(
-    var.tags
-  )
-}
+#   # Optionally, include tags directly here if not using a variable
+#   tags = merge(
+#     var.tags
+#   )
+# }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.backup_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "versioning_example" {
+#   bucket = aws_s3_bucket.backup_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
